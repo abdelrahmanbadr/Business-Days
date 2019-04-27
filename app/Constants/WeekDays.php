626ee4c1e0f0c29af1d033de048ff572906edfff
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-04-27 15:16:04 +0200

    add holidays json file to storage

---
+++ app/Constants/WeekDays.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 4/27/19
+ * Time: 3:13 PM
+ */
+
+namespace App\Constants;
+
+
+class weekDays
+{
+
+}
---
+++ app/Domain/Services/BusinessDaysCalculatorService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdelrahmanbadr
+ * Date: 4/27/19
+ * Time: 3:11 PM
+ */
+
+namespace App\Domain\Services;
+
+
+class BusinessDaysCalculatorService
+{
+
+}
