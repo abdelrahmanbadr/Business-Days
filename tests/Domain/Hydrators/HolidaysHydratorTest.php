d41247101d77f6621f6091449aaf4ca5253fe59f
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-04-27 15:36:07 +0200

    add unit test for holidays hydrator

---
+++ tests/Domain/Hydrators/HolidaysHydratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Domain\Hydrators;
+
+use App\Domain\Hydrators\HolidaysHydrator;
+use Tests\TestCase;
+use  Mockery;
+use App\Domain\Services\FileReaderService;
+use App\Domain\Models\Holiday;
+
+class HolidaysHydratorTest extends TestCase
+{
+
+    private $fileReaderMock;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->fileReaderMock = Mockery::mock(FileReaderService::class);
+    }
+
+    /** @test * */
+    function testHydrate(): void
+    {
+        $json = '[{"year": 2019,"dates": ["2019-01-01", "2019-01-02"]}]';
+        $this->fileReaderMock->shouldReceive('readFileContent')->andReturn($json);
+        $holidaysHydrator = new HolidaysHydrator($this->fileReaderMock);
+        $holidaysHydrator->hydrate();
+        $holidaysArray = [];
+        $holiday = new Holiday();
+        $holiday->setYear("2019");
+        $holiday->setDates(["2019-01-01", "2019-01-02"]);
+        $holidaysArray[] = $holiday;
+        $this->assertEquals($holidaysHydrator->hydrate(), $holidaysArray);
+    }
+
+}
